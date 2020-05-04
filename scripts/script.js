52c58da78849e7fe27dc86b46dea35624a2346e5
Alexandremma <46142888+Alexandremma@users.noreply.github.com>
2020-05-04 19:01:47 -0300

    add DeathCircle and counter

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,16 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
+const counterView = document.querySelector('p');
+let ballCounter = 0;
+
 let balls = [];
+
+let deathCircle = new DeathCircle(
+    random(0 + 10, width - 10),
+    random(0 + 10, height - 10),
+    true
+);
 
 // function to generate random number
 function random(min, max) {
@@ -12,11 +21,17 @@ function random(min, max) {
     return num;
 }
 
-function Ball(coordinateX, coordinateY, velocityX, velocityY, color, size) {
+function Shape(coordinateX, coordinateY, velocityX, velocityY, exists) {
     this.coordinateX = coordinateX;
     this.coordinateY = coordinateY;
     this.velocityX = velocityX;
     this.velocityY = velocityY;
+    this.exists = exists;
+}
+
+function Ball(coordinateX, coordinateY, velocityX, velocityY, exists, color, size) {
+    Shape.call(this, coordinateX, coordinateY, velocityX, velocityY, exists);
+
     this.color = color;
     this.size = size;
 }
@@ -32,19 +47,19 @@ Ball.prototype.update = function() {
     if ((this.coordinateX + this.size) >= width) {
         this.velocityX = -(this.velocityX);
     }
-
+    
     if ((this.coordinateX - this.size) <= 0) {
         this.velocityX = -(this.velocityX);
     }
-
+    
     if ((this.coordinateY + this.size) >= height) {
         this.velocityY = -(this.velocityY);
     }
-
+    
     if ((this.coordinateY - this.size) <= 0) {
         this.velocityY = -(this.velocityY);
     }
-
+    
     this.coordinateX += this.velocityX;
     this.coordinateY += this.velocityY;
 }
@@ -55,7 +70,7 @@ Ball.prototype.collisionDetect = function() {
             let distanceX = this.coordinateX - balls[i].coordinateX;
             let distanceY = this.coordinateY - balls[i].coordinateY;
             let ballsDistance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-
+            
             if (ballsDistance < (this.size + balls[i].size)) {
                 balls[i].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
                 balls[i].velocityX = -(balls[i].velocityX);
@@ -65,27 +80,101 @@ Ball.prototype.collisionDetect = function() {
     }
 }
 
+function DeathCircle(coordinateX, coordinateY, exists) {
+    Shape.call(this, coordinateX, coordinateY, 20, 20, exists);
+    this.color = 'white';
+    this.size = 10;
+}
+
+DeathCircle.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.coordinateX, this.coordinateY, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+DeathCircle.prototype.checkBounds = function() {
+    if ((this.coordinateX + this.size) >= width) {
+        this.coordinateX -= this.size;
+    }
+    
+    if ((this.coordinateX - this.size) <= 0) {
+        this.coordinateX += this.size;
+    }
+    
+    if ((this.coordinateY + this.size) >= height) {
+        this.coordinateY -= this.size;
+    }
+    
+    if ((this.coordinateY - this.size) <= 0) {
+        this.coordinateY += this.size;        
+    }
+}
+
+DeathCircle.prototype.setControls = function() {
+    let _this = this;
+    
+    window.onkeydown = function(e) {
+        if (e.keyCode === 65) {
+            _this.coordinateX -= _this.velocityX;
+        } else if (e.keyCode === 68) {
+            _this.coordinateX += _this.velocityX;
+        } else if (e.keyCode === 87) {
+            _this.coordinateY -= _this.velocityY;
+        } else if (e.keyCode === 83) {
+            _this.coordinateY += _this.velocityY;
+        }
+    }
+}
+
+DeathCircle.prototype.collisionDetect = function() {
+    for (let i = 0; i < balls.length; i++) {
+        if (balls[i].exists) {
+            let distanceX = this.coordinateX - balls[i].coordinateX;
+            let distanceY = this.coordinateY - balls[i].coordinateY;
+            let ballsDistance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+            if (ballsDistance < (this.size + balls[i].size)) {
+                balls[i].exists = false;
+                ballCounter--;
+            }
+        }
+    }
+}
+
+deathCircle.setControls();
+
 function loop() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
     ctx.fillRect(0, 0, width, height);
 
-    while (balls.length < 50) {
+    counterView.textContent = `Ball Count: ${ballCounter}`;
+
+    deathCircle.draw();
+    deathCircle.checkBounds();
+    deathCircle.collisionDetect();
+    
+    while (balls.length < 30) {
         let size = random(10, 20);
         let ball = new Ball(
             random(0 + size, width - size),
             random(0 + size, height - size),
-            random(-4, 4),
-            random(-4, 4),
+            random(-5, 5),
+            random(-5, 5),
+            true,
             'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
             size
-        );
-        balls.push(ball);
+            );
+            balls.push(ball);
+        ballCounter++;
     }
 
     for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        balls[i].update();
-        balls[i].collisionDetect();
+        if (balls[i].exists) {
+            balls[i].draw();
+            balls[i].update();
+        }
     }
 
     requestAnimationFrame(loop);
